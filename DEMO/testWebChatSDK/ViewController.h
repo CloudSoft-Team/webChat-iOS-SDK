95f18ba4e86bb844ddd1a5812f5e7148b3636066
Luck <Luck@MacBook-Air.local>
2018-01-19 14:11:33 +0800

    version0.0.1 不能自定义

---
+++ DEMO/testWebChatSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testWebChatSDK
+//
+//  Created by Luck on 17/6/7.
+//  Copyright © 2017年 hongmw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
